04d9d87ee017677883a01d6dbaf9aefdd043a046
ValentainFomkin <valentain1625@mail.ru>
2023-03-15 16:50:19 +0300

    create app-reducer

---
+++ src/n2-bll/app-reducer.ts
@@ -0,0 +1,13 @@
+const initialState: initialStateType = {}
+
+export const AppReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+//types
+export type initialStateType = {}
+
+export type ActionType = {}
