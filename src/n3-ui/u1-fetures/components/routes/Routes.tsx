7e87bdc52c4d105e83e674859853b225ee094d74
ValentainFomkin <valentain1625@mail.ru>
2023-03-15 23:41:46 +0300

    change routes components

---
+++ src/n3-ui/u1-fetures/components/routes/Routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {useRoutes} from "react-router-dom";
+import {Login} from "../f2-login/Login";
+import {EnterNewPassword} from "../f6-enter-new-password/EnterNewPassword";
+import {AllSuperComponents} from "../f1-super-components/AllSuperComponents";
+import {PasswordRecovery} from "../f5-password-recovery/PasswordRecovery";
+import {Profile} from "../f4-profile/Profile";
+import {Registration} from "../f3-registration/Registration";
+import {PATH} from "./paths-routes/PathRoutes";
+
+export const RoutesComponent = () => {
+
+    const routs = useRoutes([
+        {path: PATH.HOME_PAGE_PATH, element: <div>DEFAULT PAGE</div>},
+        {path: PATH.LOGIN_PATH, element: <Login/>},
+        {path: PATH.REGISTRATION_PATH, element: <Registration/>},
+        {path: PATH.PROFILE_PATH, element: <Profile/>},
+        {path: PATH.ERROR_PATH, element: <div>404: PAGE NOT FOUND</div>},
+        {path: PATH.PASSWORD_RECOVERY_PATH, element: <PasswordRecovery/>},
+        {path: PATH.ENTER_NEW_PASSWORD_PATH, element: <EnterNewPassword/>},
+        {path: PATH.SUPER_COMPONENT_TEST_PATH, element: <AllSuperComponents/>},
+    ])
+    return (
+        <>
+            {routs}
+        </>
+    );
+};
+
---
+++ src/n3-ui/u1-fetures/components/routes/paths-routes/PathRoutes.ts
@@ -0,0 +1,10 @@
+export const PATH = {
+    HOME_PAGE_PATH: '/',
+    LOGIN_PATH: '/login',
+    REGISTRATION_PATH: '/registration',
+    PROFILE_PATH: '/profile',
+    ERROR_PATH: '/404',
+    PASSWORD_RECOVERY_PATH: '/password-recovery',
+    ENTER_NEW_PASSWORD_PATH: '/enter-new-password',
+    SUPER_COMPONENT_TEST_PATH: '/super-components-test',
+}
