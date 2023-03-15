bf4ef4a2e7d0a8f7a5e10794756608ab09e9e0d5
ValentainFomkin <valentain1625@mail.ru>
2023-03-15 16:51:50 +0300

    create store.ts

---
+++ src/n2-bll/store.ts
@@ -0,0 +1,21 @@
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
+import thunkMiddleware, {ThunkDispatch} from 'redux-thunk'
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {AppReducer} from "./app-reducer";
+
+
+const rootReducer = combineReducers({
+    app: AppReducer
+})
+
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+// создаем тип диспатча который принимает как AC так и TC
+export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
+
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+// @ts-ignore
+window.store = store;
