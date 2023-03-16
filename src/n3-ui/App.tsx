da8b998770a22c6fb0e05934e0ea1534570f55f1
ValentainFomkin <valentain1625@mail.ru>
2023-03-16 14:07:59 +0300

    added useEffect

--- src/n3-ui/App.tsx
+++ src/n3-ui/App.tsx
@@ -4,12 +4,17 @@ import {NavBar} from "./u1-fetures/components/f7-navigation/NavBar";
 import s from './App.module.css'
 import {RoutesComponent} from "./u1-fetures/components/routes/Routes";
 import {BrowserRouter} from "react-router-dom";
+import {useAppSelector} from "../n2-bll/store";
 
 
 function App() {
+    const isRegister = useAppSelector(s => s.register.isRegister)
+
+
     useEffect(() => {
 
     }, [])
+
 
     return (
         <div className={s.App}>
--- src/n3-ui/u1-fetures/components/f4-profile/Profile.tsx
+++ src/n3-ui/u1-fetures/components/f4-profile/Profile.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from "../../../../n2-bll/store";
+import {isLogOutTC} from "../f2-login/login-reducer";
+import {Navigate} from "react-router-dom";
 
 export const Profile = () => {
+    const isLoggedIn = useAppSelector<boolean>(s => s.login.isLoggedIn)
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
+    }, [])
+
+    const logOutHandler = () => {
+        dispatch(isLogOutTC())
+    }
+
+    if (!isLoggedIn) {
+        return <Navigate to={'/login'}/>
+    }
     return (
         <div>
-            Profile
+            <div style={{marginBottom: '10px'}}>
+                Profile
+            </div>
+
+            {isLoggedIn &&
+                <div>
+                    <button onClick={logOutHandler}>Log Out</button>
+                </div>}
         </div>
     );
 };
