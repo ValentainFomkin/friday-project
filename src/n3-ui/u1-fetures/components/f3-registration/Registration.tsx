fd406ceaf8130254156a848f636afbeedadb3f7f
ValentainFomkin <valentain1625@mail.ru>
2023-03-17 23:16:51 +0300

    update component

--- src/n3-ui/u1-fetures/components/f3-registration/Registration.tsx
+++ src/n3-ui/u1-fetures/components/f3-registration/Registration.tsx
@@ -6,11 +6,16 @@ import Button from "@mui/material/Button";
 import {useAppDispatch, useAppSelector} from "../../../../n2-bll/store";
 import {Link, Navigate} from "react-router-dom";
 import {isRegisterTC} from "../../../../n2-bll/register-reducer";
-import {FormControl, IconButton, Input, InputAdornment, Paper, Typography} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input"
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import InputAdornment from "@mui/material/InputAdornment";
 import s from './Register.module.css'
 import {Visibility, VisibilityOff} from "@material-ui/icons";
+import {PATH} from "../routes/paths-routes/PathRoutes";
 
-export type FormikErrorType = {
+type FormikRegistrationErrorType = {
     email?: string
     password?: string
     confirmPassword?: string
@@ -32,7 +37,7 @@ export const Registration = () => {
             confirmPassword: '',
         },
         validate: values => {
-            const errors: FormikErrorType = {};
+            const errors: FormikRegistrationErrorType = {};
             if (!values.email) {
                 errors.email = 'Required';
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
@@ -66,119 +71,122 @@ export const Registration = () => {
     });
 
     if (isRegister) {
-        return <Navigate to={'/login'}/>
+        return <Navigate to={PATH.LOGIN_PATH}/>
     }
 
     return (
         <Grid container justifyContent={'center'}>
             <Grid item justifyContent={'center'}>
                 <form onSubmit={formik.handleSubmit}>
-                    <FormControl variant="standard">
-                        <Paper className={s.paper}
-                               variant={'outlined'}
-                               square>
 
-                            <Typography component={'h2'}
-                                        variant={'h4'}
-                                        marginBottom={'25px'}
-                                        fontWeight={'600'}
-                            >
-                                Sign up
+                    <Paper className={s.paper}
+                           variant={'outlined'}
+                    >
+
+                        <Typography component={'h2'}
+                                    variant={'h4'}
+                                    marginBottom={'25px'}
+                                    fontWeight={'600'}
+                        >
+                            Sign up
+                        </Typography>
+                        <div className={s.email}>
+                            <TextField
+                                variant={'standard'}
+                                fullWidth
+                                type="email"
+                                label="Email"
+                                {...formik.getFieldProps('email')}
+                                onBlur={formik.handleBlur}
+                            />
+                        </div>
+                        {formik.touched.email
+                            ? <div
+                                style={{
+                                    color: 'red',
+                                    textAlign: 'left',
+                                    marginBottom: '5px',
+                                    marginTop: '-10px'
+                                }}>{formik.errors.email}</div>
+                            : null}
+                        <div className={s.password}>
+                            <Input
+                                placeholder='Password'
+                                fullWidth
+                                id="password"
+                                type={showPassword ? 'text' : "password"}
+                                {...formik.getFieldProps('password')}
+                                onBlur={formik.handleBlur}
+                                endAdornment={
+                                    <InputAdornment position={"end"}>
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff/> : <Visibility/>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </div>
+                        {formik.touched.password
+                            ? <div style={{
+                                color: 'red',
+                                textAlign: 'left',
+                                marginBottom: '5px',
+                                marginTop: '-10px'
+                            }}>{formik.errors.password}</div>
+                            : null}
+                        <div className={s.confirmPassword}>
+                            <Input
+                                placeholder='Confirm Password'
+                                fullWidth
+                                id="confirmPassword"
+                                type={showConfirmPassword ? 'text' : 'password'}
+                                {...formik.getFieldProps('confirmPassword')}
+                                onBlur={formik.handleBlur}
+                                endAdornment={
+                                    <InputAdornment position={"end"}>
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowConfirmPassword}
+                                        >
+                                            {showConfirmPassword ? <VisibilityOff/> : <Visibility/>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                        </div>
+                        {formik.touched.confirmPassword
+                            ? <div style={{
+                                color: 'red',
+                                textAlign: 'left',
+                                marginBottom: '50px',
+                                marginTop: '-50px'
+                            }}>{formik.errors.confirmPassword}</div>
+                            : null}
+
+                        <div className={s.buttonSubmit}>
+                            <Button
+                                fullWidth
+                                variant={'contained'}
+                                type="submit">
+                                Sing up
+                            </Button>
+                        </div>
+                        <div className={s.typographyHaveAcc}>
+                            <Typography>
+                                Already have an account?
                             </Typography>
-                            <div className={s.email}>
-                                <TextField
-                                    variant={'standard'}
-                                    fullWidth
-                                    type="email"
-                                    label="Email"
-                                    {...formik.getFieldProps('email')}
-                                    onBlur={formik.handleBlur}
-                                />
-                            </div>
-                            {formik.touched.email
-                                ? <div
-                                    style={{
-                                        color: 'red',
-                                        textAlign: 'left',
-                                        marginTop: '5px'
-                                    }}>{formik.errors.email}</div>
-                                : null}
-                            <div className={s.password}>
-                                <Input
-                                    placeholder='Password'
-                                    fullWidth
-                                    id="password"
-                                    type={showPassword ? 'text' : "password"}
-                                    {...formik.getFieldProps('password')}
-                                    onBlur={formik.handleBlur}
-                                    endAdornment={
-                                        <InputAdornment position={"end"}>
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowPassword}
-                                            >
-                                                {showPassword ? <VisibilityOff/> : <Visibility/>}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                />
-                            </div>
-                            {formik.touched.password
-                                ? <div style={{
-                                    color: 'red',
-                                    textAlign: 'left',
-                                    marginTop: '5px'
-                                }}>{formik.errors.password}</div>
-                                : null}
-                            <div className={s.confirmPassword}>
-                                <Input
-                                    placeholder='Confirm Password'
-                                    fullWidth
-                                    id="confirmPassword"
-                                    type="confirmPassword"
-                                    {...formik.getFieldProps('confirmPassword')}
-                                    onBlur={formik.handleBlur}
-                                    endAdornment={
-                                        <InputAdornment position={"end"}>
-                                            <IconButton
-                                                aria-label="toggle password visibility"
-                                                onClick={handleClickShowConfirmPassword}
-                                            >
-                                                {showConfirmPassword ? <VisibilityOff/> : <Visibility/>}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    }
-                                />
-                            </div>
-                            {formik.touched.confirmPassword
-                                ? <div style={{
-                                    color: 'red',
-                                    textAlign: 'left',
-                                    marginTop: '5px'
-                                }}>{formik.errors.confirmPassword}</div>
-                                : null}
+                        </div>
+                        <div className={s.singUp}>
+                            <Link
+                                to={'/login'}>
+                                Sing in
+                            </Link>
+                        </div>
+                    </Paper>
 
-                            <div className={s.buttonSubmit}>
-                                <Button
-                                    fullWidth
-                                    variant={'contained'}
-                                    type="submit">
-                                    Submit
-                                </Button>
-                            </div>
-                            <div className={s.typographyHaveAcc}>
-                                <Typography>
-                                    Already have an account?
-                                </Typography>
-                            </div>
-                            <div className={s.singUp}>
-                                <Link
-                                    to={'/login'}>
-                                    Sing Up
-                                </Link>
-                            </div>
-                        </Paper>
-                    </FormControl>
                 </form>
             </Grid>
         </Grid>
