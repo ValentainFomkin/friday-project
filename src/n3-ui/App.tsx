c74da3f8e1307abf78fe2094be89097a4c7a4975
ValentainFomkin <valentain1625@mail.ru>
2023-03-27 19:14:47 +0300

    add new component in app bar

--- src/n3-ui/App.tsx
+++ src/n3-ui/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.module.css';
 import {NavBar} from "./u1-fetures/components/f7-navigation/NavBar";
 import s from './App.module.css'
@@ -12,25 +12,36 @@ import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
 import LinearProgress from "@mui/material/LinearProgress";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import {isLogOutTC} from "../n2-bll/login-reducer";
 import {PATH} from "./u1-fetures/components/routes/paths-routes/PathRoutes";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import avatar from './images/img-profile/avatar-profile.png'
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 
-function App() {
+export const App = () => {
     const isInitialized = useAppSelector<boolean>(s => s.app.isInitialized)
     const isLoggedIn = useAppSelector<boolean>(s => s.login.isLoggedIn)
     const status = useAppSelector<RequestStatusType>(s => s.app.status)
-
+    const actualNickName = useAppSelector<string>(s => s.app.user.name)
     const dispatch = useAppDispatch()
 
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
     useEffect(() => {
         dispatch(setInitializedTC())
     }, [])
 
 
-    const logOutHandler = () => {
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    }
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+    const handleLogOut = () => {
         dispatch(isLogOutTC())
     }
 
@@ -51,9 +62,51 @@ function App() {
                             <Link className={s.linkHomePage} to={PATH.HOME_PAGE_PATH}>INTRO</Link>
                         </Typography>
                         {isLoggedIn &&
-                            <Button onClick={logOutHandler} color="inherit">
-                                Log out
-                            </Button>
+                            <div className={s.profile}>
+                                <div className={s.profileName}>
+                                    <Link to={PATH.PROFILE_PATH}>
+                                        {actualNickName}
+                                    </Link>
+                                </div>
+                                <div className={s.profileImage}>
+                                    <IconButton
+                                        size="small"
+                                        aria-label="account of current user"
+                                        aria-controls="menu-appbar"
+                                        aria-haspopup="true"
+                                        onClick={handleMenu}
+                                        color="inherit"
+                                    >
+                                        <Avatar src={avatar}/>
+                                    </IconButton>
+                                    <Menu
+                                        id="menu-appbar"
+                                        anchorEl={anchorEl}
+                                        anchorOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'right',
+                                        }}
+                                        open={Boolean(anchorEl)}
+                                        onClose={handleClose}
+                                    >
+                                        <MenuItem onClick={handleClose}
+                                                  className={s.linkToProfile}>
+                                            <Link to={PATH.PROFILE_PATH}>
+                                                Profile
+                                            </Link>
+                                        </MenuItem>
+                                        <MenuItem onClick={handleLogOut}
+                                        >
+                                            Log out
+                                        </MenuItem>
+                                    </Menu>
+                                </div>
+                            </div>
                         }
                     </Toolbar>
                     {status === 'loading' && <LinearProgress/>}
@@ -67,4 +120,3 @@ function App() {
     );
 }
 
-export default App;
