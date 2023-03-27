1f7dab1f222cf8f3bee3d56c80aa36b028d2924b
ValentainFomkin <valentain1625@mail.ru>
2023-03-27 19:16:06 +0300

    remove user info

--- src/n2-bll/login-reducer.ts
+++ src/n2-bll/login-reducer.ts
@@ -1,35 +1,24 @@
 import {authAPI, LoginParamsType} from "../n1-dall/auth-api";
 import {Dispatch} from "redux";
-import {setAppErrorAC, setAppStatusAC, setInitializedAC} from "./app-reducer";
+import {setAppErrorAC, setAppStatusAC, setInitializedAC, setNewNamedAC, setUserEmailAC} from "./app-reducer";
 import {AxiosError} from "axios";
 import {isRegisterAC} from "./register-reducer";
 import {isForgotAC} from "./forgot-reducer";
 
 
 const initialState: InitialStateType = {
-    isLoggedIn: false,
-    user: {
-        userName: '',
-        nickName: '',
-    }
+    isLoggedIn: false
 }
 
 // CONSTANTS
 const auth_IS_LOGGED = 'auth/IS_LOGGED'
-const auth_SET_NEW_NICK_NAME = 'auth/SET_NEW_NICK_NAME'
-const auth_SET_NAME = 'auth/SET_NAME'
-
-// const SET_USER_EMAIL = 'SET_USER_EMAIL'
 
 // Reducers
 export const loginReducer = (state = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
         case "auth/IS_LOGGED":
             return {...state, isLoggedIn: action.value}
-        case "auth/SET_NEW_NICK_NAME":
-            return {...state, user: {...state.user, nickName: action.newNIckNAme}}
-        case "auth/SET_NAME":
-            return {...state, user: {...state.user, userName: action.name}}
+
         default:
             return state
     }
@@ -37,8 +26,6 @@ export const loginReducer = (state = initialState, action: ActionType): InitialS
 
 // actions
 export const isLoggedInAC = (value: boolean) => ({type: auth_IS_LOGGED, value} as const)
-export const setNewNickNamedAC = (newNIckNAme: string) => ({type: auth_SET_NEW_NICK_NAME, newNIckNAme} as const)
-export const setNameAC = (name: string) => ({type: auth_SET_NAME, name} as const)
 
 
 // thunks
@@ -50,7 +37,9 @@ export const isLoggedInTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
             dispatch(isLoggedInAC(true))
             dispatch(isRegisterAC(true))
             dispatch(setInitializedAC(true))
-            dispatch(setNameAC(data.email))
+            dispatch(setUserEmailAC(res.data.email))
+            dispatch(setNewNamedAC(res.data.name))
+
         })
         .catch((err) => {
             const error = err.response
@@ -77,18 +66,13 @@ export const isLogOutTC = () => (dispatch: Dispatch) => {
 }
 
 // types
-export type UserLoginType = {
-    userName: string
-    nickName: string
 
-}
 export type InitialStateType = {
     isLoggedIn: boolean
-    user: UserLoginType
+    // user: UserLoginType
 
 }
 export type isLoggedInActionType = ReturnType<typeof isLoggedInAC>
-export type setNewNickNamedActionType = ReturnType<typeof setNewNickNamedAC>
-export type setNamedActionType = ReturnType<typeof setNameAC>
 
-export type ActionType = isLoggedInActionType | setNewNickNamedActionType | setNamedActionType
+export type ActionType = isLoggedInActionType
+
