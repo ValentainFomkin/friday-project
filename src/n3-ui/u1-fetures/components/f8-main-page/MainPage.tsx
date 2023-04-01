0591317de2142f6e574c98e5e23a7dfee487f339
ValentainFomkin <valentain1625@mail.ru>
2023-04-01 22:05:42 +0300

    using new table

--- src/n3-ui/u1-fetures/components/f8-main-page/MainPage.tsx
+++ src/n3-ui/u1-fetures/components/f8-main-page/MainPage.tsx
@@ -2,24 +2,25 @@ import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from "../../../../n2-bll/store";
 import {PATH} from "../routes/paths-routes/PathRoutes";
 import {useNavigate} from "react-router-dom";
-import {DataTable} from "./PacksPage/PacksPageTable";
+import {PacksListTable} from "./PacksList/PacksListTable";
+
 
 export const MainPage = () => {
-    const isLoggedIn = useAppSelector<boolean>(s => s.login.isLoggedIn)
-    const navigate = useNavigate()
-    const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector<boolean>(s => s.login.isLoggedIn)
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
 
-    useEffect(() => {
-        if (!isLoggedIn) {
-            navigate(PATH.LOGIN_PATH)
-        }
-    }, [isLoggedIn])
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(PATH.LOGIN_PATH)
+    }
+  }, [isLoggedIn])
 
-    return (
-        <div>
-            {/*PACK LIST*/}
-            {/*<PacksListTable/>*/}
-            <DataTable/>
-        </div>
-    );
+  return (
+    <div>
+      {/*PACK LIST*/}
+      <PacksListTable/>
+      {/*<DataTable/>*/}
+    </div>
+  );
 };
