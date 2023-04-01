f123f977767a7dd5d362ac9e6510fec78419e5b7
ValentainFomkin <valentain1625@mail.ru>
2023-04-01 22:06:40 +0300

    new requests

--- src/n1-dall/table-api.ts
+++ src/n1-dall/table-api.ts
@@ -1,64 +1,85 @@
 import axios from 'axios'
 
+
 const instance = axios.create({
-    //baseURL: process.env.NODE_ENV === 'development'  ? 'Enter url for local backend' : 'https://neko-back.herokuapp.com/2.0/' ,
-    baseURL: 'https://neko-back.herokuapp.com/2.0/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': '1829c64f-03ce-4449-b50f-e899459dd9bd'
-    }
+  //baseURL: process.env.NODE_ENV === 'development'  ? 'Enter url for local backend' : 'https://neko-back.herokuapp.com/2.0/' ,
+  baseURL: 'https://neko-back.herokuapp.com/2.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': '1829c64f-03ce-4449-b50f-e899459dd9bd'
+  }
 })
 
 export const tableAPI = {
-    getAllPacks() {
-        return instance.get<ResponseType>('cards/pack', {
-            params: {
-                page: 1,
-                pageCount: 50
-            }
-        })
-    },
-    addNewPack(data: AddNewPackType) {
-        return instance.post<CardPacks>('cards/pack', {
-            cardsPack: {
-                name: data.name
-            }
-        })
-    }
+  getAllPacks() {
+    return instance.get<ResponseType>('cards/pack', {
+      params: {
+        page: 1,
+        pageCount: 50
+      }
+    })
+  },
+  addNewPack(data: AddNewPackType) {
+    return instance.post<CardPacks>('cards/pack', {
+      cardsPack: {
+        name: data.name
+      }
+    })
+  },
+  removePack(packID: string) {
+    return instance.delete<CardPacks>('cards/pack', {
+      params: {
+        id: packID
+      }
+    })
+  },
+  updatePack(data: UpdateCardPack) {
+    return instance.put<CardPacks>('cards/pack', {
+      cardsPack: {
+        _id: data._id,
+        name: data.name
+      }
+    })
+  }
 
 }
 
 //types
 export type AddNewPackType = {
-    name: string
+  name: string
+}
+
+export type UpdateCardPack = {
+  _id: string
+  name: string
 }
 
 export type PackConfigType = {
-    packName?: string // не обязательно
-    min?: number // не обязательно
-    max?: number // не обязательно
-    page?: number // не обязательно
-    pageCount?: number // не обязательно
-    user_id?: string// чьи колоды не обязательно, или придут все
-    block?: boolean
+  packName?: string // не обязательно
+  min?: number // не обязательно
+  max?: number // не обязательно
+  page?: number // не обязательно
+  pageCount?: number // не обязательно
+  user_id?: string// чьи колоды не обязательно, или придут все
+  block?: boolean
 }
 
 export type ResponseType = {
-    cardPacks: CardPacks[]
-    cardPacksTotalCount: number// количество колод
-    maxCardsCount: number
-    minCardsCount: number
-    page: number // выбранная страница
-    pageCount: number
+  cardPacks: CardPacks[]
+  cardPacksTotalCount: number// количество колод
+  maxCardsCount: number
+  minCardsCount: number
+  page: number // выбранная страница
+  pageCount: number
 
 }
 export type CardPacks = {
-    _id: string
-    user_id: string
-    user_name: string
-    name: string
-    cardsCount: number
-    created: string
-    updated: string
-    private: boolean
+  _id: string
+  user_id: string
+  user_name: string
+  name: string
+  cardsCount: number
+  created: string
+  updated: string
+  private: boolean
 }
