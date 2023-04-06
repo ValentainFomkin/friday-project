a60b786469c91ba2e463692f7ab7dc442a0c132a
ValentainFomkin <valentain1625@mail.ru>
2023-04-06 22:50:58 +0300

    updated get request

--- src/n1-dall/auth-api.ts
+++ src/n1-dall/auth-api.ts
@@ -1,102 +1,101 @@
-import axios from 'axios'
-import {CorrectHostPath} from "../n3-ui/u1-fetures/components/f5-password-recovery/PasswordRecovery";
+import {
+  CorrectHostPath
+} from "../n3-ui/u1-fetures/components/f5-password-recovery/PasswordRecovery";
+import {instance} from "../n3-ui/u1-fetures/c1-utils/Instance/instance";
 
-const instance = axios.create({
-    //baseURL: process.env.NODE_ENV === 'development'  ? 'Enter url for local backend' : 'https://neko-back.herokuapp.com/2.0/' ,
-    baseURL: 'https://neko-back.herokuapp.com/2.0/',
-    withCredentials: true,
-    headers: {
-        'API-KEY': '1829c64f-03ce-4449-b50f-e899459dd9bd'
-    }
-})
 
 export const authAPI = {
-    register(data: RegisterType) {
-        return instance.post<{ error?: string }>('auth/register', data)
-    },
-    login(data: LoginParamsType) {
-        return instance.post<ResponseType>('auth/login', data)
-    },
-    logOut() {
-        return instance.delete<LogOutResponseType>('auth/me')
-    },
-    forgot(email: ForgotType, envPath: CorrectHostPath) {
-        const data = {
-            email: email.email,
-            message: `<h1>Перейдите по ссылке для восстановления пароля: <a href='${envPath}#/set-new-password/$token$'>link</a></h1>`,
-        }
-        return instance.post<ForgotResponseType>('auth/forgot', data)
-    },
-    me() {
-        return instance.post<ResponseType>('auth/me')
-    },
-    setNewPassword(data: SetNewPassType) {
-        return instance.post<ForgotResponseType>('auth/set-new-password', data)
-    },
-    changeProfileInfo(data: ProfileInfoType) {
-        return instance.put<UpdateUserResponseType>('auth/me', data)
+  register(data: RegisterType) {
+    return instance.post<{ error?: string }>('auth/register', data)
+  },
+  login(data: LoginParamsType) {
+    return instance.post<ResponseType>('auth/login', data)
+  },
+  logOut() {
+    return instance.delete<LogOutResponseType>('auth/me')
+  },
+  forgot(email: ForgotType, envPath: CorrectHostPath) {
+    const data = {
+      email: email.email,
+      from: `test-front-admin <ai73a@yandex.by>`,
+      message: `<a href='${envPath}#/set-new-password/$token$'>link</a>`,
     }
+    return instance.post<ForgotResponseType>('auth/forgot', {
+      email: data.email,
+      from: data.from,
+      message: data.message
+    })
+  },
+  me() {
+    return instance.post<ResponseType>('auth/me')
+  },
+  setNewPassword(data: SetNewPassType) {
+    return instance.post<ForgotResponseType>('auth/set-new-password', data)
+  },
+  changeProfileInfo(data: ProfileInfoType) {
+    return instance.put<UpdateUserResponseType>('auth/me', data)
+  }
 
 
 }
 
 //types
 export type RegisterType = {
-    email: string
-    password: string
+  email: string
+  password: string
 }
 
 export type ProfileInfoType = {
-    name: string
-    avatar?: string
+  name: string
+  avatar?: string
 }
 
 export type SetNewPassType = {
-    password?: string
-    resetPasswordToken?: string
+  password?: string
+  resetPasswordToken?: string
 }
 
 export type ForgotType = {
-    email: string
-    // from: string
-    // message: string
+  email: string
+  // from: string
+  // message: string
 }
 
 export type ForgotResponseType = {
-    info: string
-    error: string
+  info: string
+  error: string
 }
 
 export type LoginParamsType = {
-    email: string
-    password: string
-    rememberMe: boolean
+  email: string
+  password: string
+  rememberMe: boolean
 }
 
 export type LogOutResponseType = {
-    info: string
-    error: string
+  info: string
+  error: string
 }
 
 export type UpdateUserResponseType = {
-    updatedUser: ResponseType
-    error?: string
+  updatedUser: ResponseType
+  error?: string
 }
 
 export type ResponseType = {
-    _id: string
-    email: string
-    name: string
-    avatar?: string
-    publicCardPacksCount: number
+  _id: string
+  email: string
+  name: string
+  avatar?: string
+  publicCardPacksCount: number
 // количество колод
-    token: string
-    created: Date
-    updated: Date
-    isAdmin: boolean
-    verified: boolean // подтвердил ли почту
-    rememberMe: boolean
+  token: string
+  created: Date
+  updated: Date
+  isAdmin: boolean
+  verified: boolean // подтвердил ли почту
+  rememberMe: boolean
 
-    error?: string
+  error?: string
 
 }
